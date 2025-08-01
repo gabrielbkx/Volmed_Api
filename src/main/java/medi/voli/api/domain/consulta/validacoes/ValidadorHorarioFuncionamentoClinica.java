7c2bfe037d217fd8f4a35bb1e8bdc44067449b92
Gabriel <oliveirafrerreira97@hotmail.com>
2025-08-01 18:03:32 -0300

    criação da classe que valida o horario e dia de funcionamento da clínica

---
+++ src/main/java/medi/voli/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,21 @@
+package medi.voli.api.domain.consulta.validacoes;
+
+import medi.voli.api.domain.ValidacaoException;
+import medi.voli.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.dataHoraConsulta();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoExpediente = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoExpediente){
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica. ")
+        }
+    }
+}
