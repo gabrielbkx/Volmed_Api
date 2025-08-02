2136238012ce56116c759b87d7de42aa8dd4c757
Gabriel <oliveirafrerreira97@hotmail.com>
2025-08-02 00:33:41 -0300

    atualização da classe OpenApi

--- src/main/java/medi/voli/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/medi/voli/api/infra/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,7 @@ package medi.voli.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,19 @@ public class SpringDocConfigurations {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().components(new Components()
-                .addSecuritySchemes("bearer-key", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Volimed API") // <-- Nome da sua API aqui
+                        .version("1.0.0")
+                        .description("API de gerenciamento de uma clínica")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
